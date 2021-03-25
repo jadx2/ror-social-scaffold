c175e2f196f389955ca438ba5579be7edc7ba80e
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-03-26 00:38:08 +0500

    Remove logic from application view file

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,15 @@ class User < ApplicationRecord
                             dependent: :destroy
 
   has_many :friends, -> { merge(Friendship.friends) }, through: :friend_sent, source: :sent_to, dependent: :destroy
-
   has_many :notifications, dependent: :destroy
+
+  # review
+  # has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
+  # has_many :friends, through: :confirmed_friendships
+
+  # has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  # has_many :pending_friends, through: :pending_friendships, source: :friend
+
+  # has_many :inverted_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id"
+  # has_many :friend_requests, through: :inverted_friendships
 end
